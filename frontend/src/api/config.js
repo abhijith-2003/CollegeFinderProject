88cef482d85aed8570c6d093a54eb4021ee66b7a
abhijith-2003 <abhijith4667@gmail.com>
2025-10-25 13:23:22 +0530

    College Finder FInal Code

---
+++ frontend/src/api/config.js
@@ -0,0 +1,5 @@
+// config.js
+export const API_CONFIG = {
+  BASE_URL: 'https://college-finder-project-v56y.vercel.app'
+
+}
---
+++ frontend/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        // 🔗 Use your deployed FastAPI backend URL here
+        target: 'https://college-finder-project-v56y.vercel.app',
+        changeOrigin: true,
+        secure: true, // ensures HTTPS backend connections
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      },
+    },
+  },
+})
